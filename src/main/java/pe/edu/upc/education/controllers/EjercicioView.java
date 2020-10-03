33c22fa61df73265f976b622fda76acad197f595
MichelT20T <micheltaipe20@gmail.com>
2020-10-03 13:58:23 -0500

    TP20

---
+++ src/main/java/pe/edu/upc/education/controllers/EjercicioView.java
@@ -0,0 +1,65 @@
+
+package pe.edu.upc.education.controllers;
+
+
+import java.io.Serializable;
+
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pe.edu.upc.education.models.entities.Ejercicio;
+import pe.edu.upc.education.models.services.EjercicioService;
+
+@Named("ejercicioView")
+@ViewScoped
+public class EjercicioView implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EjercicioService ejercicioService;
+	
+	
+	private Ejercicio ejercicio;		
+	
+
+	
+	
+	public void cleanForm() {
+		this.ejercicio = new Ejercicio();
+	}
+	
+	public void newEjercicio() {
+		try {
+			ejercicioService.save(this.ejercicio);
+			cleanForm();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		this.addMessage("Se creó el ejercicio satisfactoriamente");
+	}
+	
+
+	
+	public void addMessage(String summary) {		
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }	
+	
+	public Ejercicio getEjercicio() {
+		return ejercicio;
+	}
+
+	public EjercicioService getEjercicioService() {
+		return ejercicioService;
+	}
+
+	
+
+}
