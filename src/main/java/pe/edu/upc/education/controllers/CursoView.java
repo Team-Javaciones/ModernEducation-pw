b6ad51a4def279a633ea2a1f51a3eb9102f59f84
Diego-Pz <diegopaz25814@gmail.com>
2020-10-03 12:35:12 -0500

    TP v1

--- src/main/java/pe/edu/upc/education/controllers/CursoView.java
+++ src/main/java/pe/edu/upc/education/controllers/CursoView.java
@@ -14,12 +14,25 @@ import pe.edu.upc.education.models.entities.Alumno;
 import pe.edu.upc.education.models.entities.Curso;
 import pe.edu.upc.education.models.services.AlumnoService;
 import pe.edu.upc.education.models.services.CursoService;
+import pe.edu.upc.education.utils.Action;
 
 @Named("cursoView")
 @ViewScoped
 public class CursoView implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+
+	private List<Curso> cursos;
+	private Curso cursoSearch;
+	
+	private Action action;
+	
+	private String styleTableCurso;
+	private String styleTableAsesor;
+	private String styleSearchCurso;
+	private String styleSearchAsesor;
+	
+	
 	
 	@Inject
 	private AlumnoService alumnoService;
@@ -35,11 +48,54 @@ public class CursoView implements Serializable{
 	public void init() {
 		this.cleanForm();
 		this.loadAlumnos();
-		
+		this.cursoSearch=new Curso();
+		this.action=Action.NONE;
+		this.stateListCurso();
 	}
 	public void cleanForm()
 	{
 		this.curso=new Curso();
+	}
+	
+	public void loadCursos() {
+		try {
+			this.cursos=cursoService.findAll();
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void searchAsesor() {
+		cleanForm();
+		this.action=Action.NONE;
+		this.stateListAsesor();
+	}
+	
+	public void findByNombreCurso() {
+		if(!this.cursoSearch.getNombre().isEmpty()) {
+			try {
+				this.cursos=cursoService.findByNombre(cursoSearch.getNombre());
+				this.stateListCurso();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
+	public void stateListCurso() {
+		this.styleTableCurso="display:block;";
+		this.styleTableAsesor="display:none;";
+		this.styleSearchCurso="display:block;";
+		this.styleSearchAsesor="display:none;";
+	}
+	
+	public void stateListAsesor() {
+		this.styleTableCurso="display:none;";
+		this.styleTableAsesor="display:block;";
+		this.styleSearchCurso="display:none;";
+		this.styleSearchAsesor="display:block;";
 	}
 	
 	public void newCurso()
@@ -80,6 +136,31 @@ public class CursoView implements Serializable{
 		}
 	}
 
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+
+	public Curso getCursoSearch() {
+		return cursoSearch;
+	}
+
+	public String getStyleTableCurso() {
+		return styleTableCurso;
+	}
+
+	public String getStyleTableAsesor() {
+		return styleTableAsesor;
+	}
+
+	public String getStyleSearchCurso() {
+		return styleSearchCurso;
+	}
+
+
+	public String getStyleSearchAsesor() {
+		return styleSearchAsesor;
+	}
+	
 	public AlumnoService getAlumnoService() {
 		return alumnoService;
 	}
